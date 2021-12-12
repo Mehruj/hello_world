ec79cc88926e22cfcb232a438c8c3c2835779d45
Mehruj <49351612+Mehruj@users.noreply.github.com>
2021-12-12 16:39:11 -0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,13 @@
 const { request } = require("express");
 const jwt = require("jsonwebtoken");
+const autho= process.env.SECRET_JWT;
 require("dotenv").config();
 const Auth = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ errorMessage: "unauthorized User" });
   }
-  const verified = jwt.verify(token, process.env.SECRET_JWT);
+  const verified = jwt.verify(token, autho);
   req.user = verified.user;
 
   next();
