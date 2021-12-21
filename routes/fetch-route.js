91fa61587d90f25279bb7015c0f434497a639cb2
Mehruj <49351612+Mehruj@users.noreply.github.com>
2021-12-21 15:00:40 -0500

    Delete routes directory

--- routes/fetch-route.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var fetchController = require("../controllers/fetch-controller");
-
-router.get("/fetch-data", fetchController.fetchData);
-
-module.exports = router;
